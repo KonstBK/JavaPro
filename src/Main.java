eda31e1099cf19a5bdf6de383a7ee4e6859b5fe6
KonstBK <boyko.konstantin@yahoo.com>
2022-09-19 21:21:14 +0300

    added deposit calculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bank.calculateDeposit(1);
+        Bank.calculateDeposit(5);
+        Bank.calculateDeposit(10);
     }
 }
